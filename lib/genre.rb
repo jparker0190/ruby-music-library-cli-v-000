c7b2cc35e065c7c3d3415d0ad0071e3fb652cd0c
Joseph Parker <jparker010190@gmail.com>
2018-05-31 23:29:17 +0000

    automatically backed up by learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,21 +1,43 @@
 class Genre
-  @@all = []
   attr_accessor :name
+
+  extend Concerns::Findable::ClassMethods
+
+  @@all = []
+
   def initialize(name)
     @name = name
+    @songs = []
   end
+
   def self.all
     @@all
   end
+
   def self.destroy_all
     @@all.clear
   end
+
+  def self.create(name)
+    genre = Genre.new(name)
+    genre.save
+    genre
+  end
+
   def save
     @@all << self
   end
-  def self.create(name)
-    yeppers = self.new(name)
-    yeppers.save
-    yeppers
+
+  def add_song(song)
+    song.genre = self if song.genre.nil?
+    self.songs << song unless self.songs.include?(song)
+  end
+
+  def songs
+    @songs
+  end
+
+  def artists
+    @songs.collect { |song| song.artist }.uniq
   end
 end
